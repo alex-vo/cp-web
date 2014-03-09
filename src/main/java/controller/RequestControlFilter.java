92ef5df0e00a3ac3cb9746d523e25d3fc96cbbeb
Alex <avoroncovs@gmail.com>
2014-03-09 02:07:44 +0200

    Optimized imports

--- src/main/java/controller/RequestControlFilter.java
+++ src/main/java/controller/RequestControlFilter.java
@@ -1,8 +1,13 @@
 package controller;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.FilterChain;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.HashSet;
